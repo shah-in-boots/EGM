e3a03b21a8f9485d8dc1edb3a5476ce0d3c7ade1
Anish Shah <asshah4@users.noreply.github.com>
2023-01-17 20:32:40 -0600

    major peaks are viable #13

--- R/intervals.R
+++ R/intervals.R
@@ -1,5 +1,6 @@
 #' Finds peaks that represent near-field signal
 #' @param hz the frequency that the signal was sampled on. Default is 1000/sec.
+#' @name intervals
 #' @export
 find_major_peaks <- function(x, hz = 1000L, sensitivity = .2) {
 	stopifnot("Should be a double vector" =
@@ -35,5 +36,52 @@ find_major_peaks <- function(x, hz = 1000L, sensitivity = .2) {
 
 	# Return list
 	list(peaks = pks$pks, location = pks$loc)
+
+}
+
+#' Find minor peaks that may be present as near-field signal
+#' @rdname intervals
+#' @export
+find_minor_peaks <- function(x, hz = 1000L, sensitivity = 0.2, specificity = 0.8) {
+	stopifnot("Should be a double vector" =
+							typeof(x) == "double")
+
+	if (inherits(x, "eps")) {
+		hz <- attr(x, "frequency")
+	}
+
+	y <- as.numeric(x)
+
+	# Major peaks physiologically are not usually faster than 200 ms
+	# However, minor peaks may occur much closer to a major peak
+	min_dist <- 0.03 * hz
+
+	# The distance between signals should be relatively small, like 20-30 ms
+	min_width <- 0.03 * hz
+
+	# Major peak height should not vary too much for EGMs
+	# Minor peaks must be "shorter" than major peaks by some percent
+	max_ht <- specificity * max(abs(y), na.rm = TRUE)
+
+	# Minimum height is to remove artifact
+	min_ht <- sensitivity * max(abs(y), na.rm = TRUE)
+
+	# Get all the individual peak locations and return them as index of position
+	# Position as part of initial vector
+	pks <-
+		gsignal::findpeaks(
+			data = y,
+			MinPeakDistance = min_dist,
+			MinPeakWidth = min_width,
+			MinPeakHeight = min_ht,
+			DoubleSided = TRUE
+		)
+
+	plot(y, type = 'l')
+	points(pks$loc, pks$pks, type = 'p', col = 'orange')
+
+	# Return list
+	list(peaks = pks$pks, location = pks$loc)
+
 
 }
--- tests/testthat/test-intervals.R
+++ tests/testthat/test-intervals.R
@@ -1,44 +1,11 @@
 test_that("can find peaks in a single channel", {
 	df <- read_lspro(file = test_path("sample-egm.txt"))
-
-	hz <- attr(df$I, "frequency")
+	pks <- find_major_peaks(df$I)
 	sig <- as.numeric(df$I)
-	sig <- as.numeric(df$`HIS D`)
+	expect_length(pks, 2)
+	expect_type(pks, "list")
 
-	plot(sig, type = "l")
-	plot(df$I, type = "l")
-	plot(df$`CS 9-10`, type = "l")
-	plot(df$`HIS D`, type = "l")
+	plot(sig, type = "l", col = "black")
+	points(pks$location, pks$peaks, type = "p", col = "orange")
 
-	peaks <-
-		gsignal::findpeaks(
-			sig,
-			MinPeakDistance = 200,
-			MinPeakHeight = max(abs(sig), na.rm = TRUE) / 5,
-			MinPeakWidth = 20,
-			DoubleSided = TRUE
-		)
-	plot(sig, type = "l")
-	points(peaks$loc, peaks$pks, col = "orange", pch = 1)
-
-	# Subset data for an annotation channel
-	ann <- dt[label == channel]
-	t <- ann$index
-	peaks <-
-		gsignal::findpeaks(
-			ann$mV,
-			MinPeakHeight = quantile(ann$mV, probs = 0.99),
-			MinPeakDistance = minimum_interval,
-			DoubleSided = TRUE
-		)
-
-	# Find peaks and intervals, and trim data for mapping purposes
-	pk_locs <- ann$time[peaks$loc]
-	ints <- diff(peaks$loc)
-	ints_locs <- pk_locs[1:length(ints)] + ints/(2 * hz)
-	ht <- mean(abs(peaks$pks), na.rm = TRUE)
-
-
-	dt_locs <- round(ints_locs*hz, digits = 0)
-	dt_ann <- ann[dt_locs, ]
 })
