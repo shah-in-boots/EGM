75c545cb9d5b157e5522d8e6246057042a49b723
Anish Shah <asshah4@users.noreply.github.com>
2023-04-06 22:33:49 -0500

    start peak detection

---
+++ R/peaks.R
@@ -0,0 +1,63 @@
+#' EGM waveform detection
+#' @export
+
+
+
+#' ECG waveform detection
+#'
+#' @return Creates a WFDB-compatible annotation file
+#'
+#' @inheritParams write_wfdb
+#'
+#' @param detector Signal detector that can create WFDB-compatible annotation
+#'   files
+#'
+#' @examples
+#' record <- "sample"
+#' detector <- "ecgpuwave"
+#' @export
+detect_surface_beats <- function(record,
+																 record_dir = ".",
+																 detector,
+																 wfdb_path,
+																 ...) {
+
+	# Validate
+	# 	WFDB software - must be an ECG detector software
+	#		WFDB must be on path
+	# 	Reading/writing directory must be on path
+	if (checkmate::check_subset(detector, c("gqrs",
+																					"sqrs",
+																					"sqrs125",
+																					"wqrs",
+																					"ecgpuwave"))) {
+		ext <- "qrs"
+	}
+
+	cmd <- find_wfdb_software(wfdb_path, detector)
+
+	if (fs::dir_exists(record_dir)) {
+		wd <- fs::path(record_dir)
+	} else {
+		wd <- getwd()
+	}
+
+	rec <- paste("-r", record)
+	ann <- paste("-a", ext)
+
+	# Change working directory for writing purposes
+	# 	This should change back at end of writing process
+	withr::with_dir(new = wd,
+									code = {
+										# System call to beat detector/annotator
+										system2(command = cmd,
+														args = c(rec, ann),
+														stdout = FALSE)
+
+										# Clean up extraneous files that were created
+										# 	`ecgpuwave` -> fortran files
+										fs::file_delete(c("fort.21", "fort.20"))
+									})
+
+
+}
